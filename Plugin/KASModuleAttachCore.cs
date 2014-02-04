63e545ef6d1d59128e1ce10f84354873939375d4
Alexander Gavrilov <angavrilov@gmail.com>
2014-02-05 01:07:46 +0400

    When docking with KAS, let the biggest vessel keep its control state.

--- Plugin/KASModuleAttachCore.cs
+++ Plugin/KASModuleAttachCore.cs
@@ -334,6 +334,7 @@ namespace KAS
                 {
                     KAS_Shared.DebugLog("DockTo(Core) Switching focus to " + this.part.vessel.vesselName);
                     FlightGlobals.ForceSetActiveVessel(this.part.vessel);
+                    FlightInputHandler.ResumeVesselCtrlState(this.part.vessel);
                 }
                 otherAttachModule.part.Couple(this.part);
             }
@@ -344,12 +345,11 @@ namespace KAS
                 {
                     KAS_Shared.DebugLog("DockTo(Core) Switching focus to " + otherAttachModule.part.vessel.vesselName);
                     FlightGlobals.ForceSetActiveVessel(otherAttachModule.part.vessel);
+                    FlightInputHandler.ResumeVesselCtrlState(otherAttachModule.part.vessel);
                 }
                 this.part.Couple(otherAttachModule.part);
             }
 
-            this.vessel.ctrlState = new FlightCtrlState();
-            FlightInputHandler.SetNeutralControls();
             GameEvents.onVesselWasModified.Fire(this.part.vessel);
         }
 
